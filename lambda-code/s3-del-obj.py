9d5c9f53b22a540f24a1c8abef237e8df2461058
YU88John <mtk.john88@gmail.com>
2023-12-22 22:06:13 +0400

    Cloudformation and delete object function

---
+++ lambda-code/s3-del-obj.py
@@ -0,0 +1,24 @@
+import os
+import boto3
+
+def lambda_handler(event, context):
+    bucket_name = os.environ.get('S3_BUCKET_NAME')
+
+    if not bucket_name:
+        return {
+            'statusCode': 400,
+            'body': 'S3_BUCKET_NAME environment variable not set'
+        }
+
+    # Create an S3 client
+    s3 = boto3.client('s3')
+
+    objects = s3.list_objects_v2(Bucket=bucket_name)
+    if 'Contents' in objects:
+        for obj in objects['Contents']:
+            s3.delete_object(Bucket=bucket_name, Key=obj['Key'])
+    
+    return {
+        'statusCode': 200,
+        'body': f'All objects deleted from S3 bucket: {bucket_name}'
+    }
